e748f1405a7191b6305bcdd1d650c4d921ca1cc4
jsreeram1958 <tvkramca@gmail.com>
2016-09-24 18:47:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,7 @@ console.log('Loaded!');
 //change the text of main-text
 var element = document.getElementById('main-text');
 element.innerHTML = "New Value 1";
+
+//move the image
+var img = document.getElementById('madi');
+img.onclick;
