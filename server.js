8934684370070eb9ec5a544ee4151882cc004198
jsreeram1958 <tvkramca@gmail.com>
2016-09-24 14:59:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ var content = {
                       This is the content of my Article One.   This is the content of my Article One.                 
                 </p>`
             };
+            
+            
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
