bf99a065c8bb1ad6e1eb9a3fb1c89b6dd5458f5d
jsreeram1958 <tvkramca@gmail.com>
2016-09-24 15:25:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ function createTemplate (data) {
                 </div>
                 
                 <div>
-                    $[content} 
+                    ${content} 
                 </div>  
             </div>
                 
