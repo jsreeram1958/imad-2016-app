1edd296c5b526abd0ff02aff74068ce459433d8e
jsreeram1958 <tvkramca@gmail.com>
2016-09-24 20:30:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,8 +4,13 @@ var element = document.getElementById('main-text');
 element.innerHTML = "New Value 1";
 
 //move the image
-var img = document.getElementById('madi');
+var marginLeft = 0;
 
-img.onclick = function () {
-    img.style.marginLeft = '100px';
-    };
+    function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+    }
+  
+    img.onclick = function () {		   
+      var interval = setInterval(moveRight, 100); 
+    }; 		  
