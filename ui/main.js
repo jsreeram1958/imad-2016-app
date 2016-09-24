6f45c8f9ed702df2ed573b9463e7c6ff8df87509
jsreeram1958 <tvkramca@gmail.com>
2016-09-24 18:29:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
 console.log('Loaded!');
 //change the text of main-text
-var element = document.getElementById(main-text);
+var element = document.getElementById('main-text');
 element.InnerHTML = "New Value 1";
