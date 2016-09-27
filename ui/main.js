71817aece67cb3dbf65ab9108e5d12ae07eba610
Kanakaram T V <tvkramca@gmail.com>
2016-09-27 14:31:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,17 @@
 console.log('Loaded!');
 //change the text of main-text
-var element = document.getElementById('main-text');
-element.innerHTML = "New Value 1";
+//var element = document.getElementById('main-text');
+//element.innerHTML = "New Value 1";
 
 //move the image
-var img = document.getElementById('madi');
-var marginLeft = 0;
+//var img = document.getElementById('madi');
+//var marginLeft = 0;
 
-    function moveRight () {
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft + 'px';
-    }
+//    function moveRight () {
+//    marginLeft = marginLeft + 5;
+//    img.style.marginLeft = marginLeft + 'px';
+//    }
   
-    img.onclick = function () {		   
-      var interval = setInterval(moveRight,50); 
-    }; 		  
+//    img.onclick = function () {		   
+//      var interval = setInterval(moveRight,50); 
+//    }; 		  
