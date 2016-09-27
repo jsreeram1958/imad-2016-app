8ad1f8ac3d9fbe012e77e9c65d5d8844775f9134
Kanakaram T V <tvkramca@gmail.com>
2016-09-27 12:50:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,5 +13,5 @@ var marginLeft = 0;
     }
   
     img.onclick = function () {		   
-      var interval = setInterval(moveRight, 50); 
+      var interval = setInterval(moveRight,50); 
     }; 		  
