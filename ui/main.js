d442292e3411c2741fde757e76fac3a6e6c2f85b
Kanakaram T V <tvkramca@gmail.com>
2016-09-28 11:38:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -48,20 +48,32 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 
 submit.onclick = function () {
-  var names = ['name1', 'name2', 'name3', 'name4'];
-  var list = '';
-  
-  for (var i=0; i < names.length; i++) {
-      list += '<li>' + names[i] +'</li>';
-  }
-  
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
     
+     //Create a request object
+   var request = new XMLHttpRequest();
+   
+   //Capture the request and store in a variable
+   request.onreadystatechange = function () {
+      if (request.readyState == XMLHttpRequest.DONE) {
+        if (request.status == 200) {
+            var names = request.responseText;
+            names=JSON.parse(names);
+            var list = '';
+  
+            for (var i=0; i < names.length; i++) {
+                list += '<li>' + names[i] +'</li>';
+             }
+  
+            var ul = document.getElementById('namelist');
+            ul.innerHTML = list; 
+         }
+       }
 };
 
-
-
+ //Make a request
+    request.open ('GET', 'http://jsreeram1958.imad.hasura-app.io/submit-name?name=' +name, true);
+    request.send(null);
+};
 
 
 
